f43e34c55a690011489836b675fb83a90bdfe249
fko2247018 <fko2247018@FIC-PCROOM3B-14>
2022-09-27 12:06:56 +0900

    プロジェクト ファイルを追加します。

---
+++ Game.h
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace Game
+{
+	// ウィンドーモード設定
+	constexpr bool kWindowMode = true;
+	// ウィンドウ名
+	const char* const kTitleText = "ゲーム名";
+	// ウィンドウサイズ
+	constexpr int kScreenWidth = 1280;
+	constexpr int kScreenHeight = 720;
+	// カラーモード
+	constexpr int kColorDepth = 32;
+
+
+};
---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include "DxLib.h"
+
+#include "Game.h"
+// プログラムは WinMain から始まります
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	// Windowモード設定
+	ChangeWindowMode(Game::kWindowMode);
+	// ウィンドー名設定
+	SetMainWindowText(Game::kTitleText);
+	// 画面サイズ設定
+	SetGraphMode(Game::kScreenWidth, Game::kScreenHeight, Game::kColorDepth);
+
+	if (DxLib_Init() == -1)		// ＤＸライブラリ初期化処理
+	{
+		return -1;			// エラーが起きたら直ちに終了
+	}
+	// ダブルバッファモード
+	SetDrawScreen(DX_SCREEN_BACK);
+
+
+	while (ProcessMessage() == 0)
+	{
+		LONGLONG time = GetNowHiPerformanceCount();
+		// 画面のクリア
+		ClearDrawScreen();
+
+		DrawString(0, 0,"テスト！", GetColor(0, 255, 255));
+
+		// 裏画面を表画面を入れ替える
+		ScreenFlip();
+
+		if (CheckHitKey(KEY_INPUT_ESCAPE)) break;
+
+		while (GetNowHiPerformanceCount() - time < 16667)
+		{
+
+		}
+	}
+
+	DxLib_End();				// ＤＸライブラリ使用の終了処理
+
+	return 0;				// ソフトの終了 
+}
